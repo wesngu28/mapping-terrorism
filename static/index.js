a564f038ed0feafea1eb4e510809955fa8c0b3f3
wesngu028 <serpents123@gmail.com>
2022-05-25 14:24:22 -0700

    ultra simple page switcher

---
+++ static/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const navButtons = document.querySelectorAll('nav button');
+navButtons.forEach(elem => elem.addEventListener('click', pages))
+
+function pages() {
+    const navButtons = document.querySelectorAll('nav button');
+    navButtons.forEach(elem => elem.classList.remove('selected'));
+    this.classList.add('selected');
+
+    if(this.id === 'home') {
+        const allOverview = document.querySelectorAll('section');
+        allOverview.forEach(elem => elem.classList.add('hidden'));
+        const overview = document.getElementById('overview');
+        overview.classList.remove('hidden');
+    }
+    if(this.id === 'gtd') {
+        const allOverview = document.querySelectorAll('section');
+        allOverview.forEach(elem => elem.classList.add('hidden'));
+        const map = document.getElementById('map-holder');
+        map.classList.remove('hidden');
+    }
+}
