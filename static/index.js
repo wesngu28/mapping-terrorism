b12cb9d2ebf5a7934ad0b2a486b7b660743c86f3
Wesley <serpents123@gmail.com>
2022-06-07 15:41:14 -0700

    overview, display changes, scss

--- static/index.js
+++ static/index.js
@@ -10,7 +10,13 @@ function pages() {
 
     if(this.id === 'home') {
         const allOverview = document.querySelectorAll('section');
-        allOverview.forEach(elem => elem.classList.add('hidden'));
+        allOverview.forEach((elem) => {
+            if(elem.id === 'map-holder') {
+                elem.classList.add('map-hidden')
+            } else {
+                elem.classList.add('hidden')
+            }
+        });
         const overview = document.getElementById('overview');
         overview.classList.remove('hidden');
     }
@@ -19,5 +25,6 @@ function pages() {
         allOverview.forEach(elem => elem.classList.add('hidden'));
         const map = document.getElementById('map-holder');
         map.classList.remove('hidden');
+        map.classList.remove('map-hidden');
     }
 }
